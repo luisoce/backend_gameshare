3dc75cd55e415bc0fb87838f3d02b45b20aa41a9
luisoce <luis.campos.escobar@gmail.com>
2024-07-04 15:37:05 -0400

    Modifica seguridad

--- src/main/java/com/proyectoIntegrador/gameShare/configuracion/SeguridadConfig.java
+++ src/main/java/com/proyectoIntegrador/gameShare/configuracion/SeguridadConfig.java
@@ -16,6 +16,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 // Clase que contiene la configuración relacionada con la seguridad.
 @Configuration
@@ -41,6 +46,20 @@ public class SeguridadConfig {
     @Bean
     JwtFiltroDeAutenticacion jwtFiltroDeAutenticacion() {
         return new JwtFiltroDeAutenticacion();
+    }
+
+    // Bean para configurar CORS
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://frontendgameshare.up.railway.app"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     // Bean para establecer una cadena de filtros de seguridad en la app. Aquí se determinan los permisos según rol del usuario.
