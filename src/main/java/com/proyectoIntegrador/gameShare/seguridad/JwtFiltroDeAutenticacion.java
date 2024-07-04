e80329e04ccdafe244f975763f175c391e597e5d
luisoce <luis.campos.escobar@gmail.com>
2024-07-04 13:24:20 -0400

    Modifica JwFiltroDeAutenticacion

--- src/main/java/com/proyectoIntegrador/gameShare/seguridad/JwtFiltroDeAutenticacion.java
+++ src/main/java/com/proyectoIntegrador/gameShare/seguridad/JwtFiltroDeAutenticacion.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,6 +21,7 @@ import java.util.List;
 
 // Valida la info del token y, de ser exitoso, establece la autenticación del usuario.
 @Data
+@Component
 public class JwtFiltroDeAutenticacion extends OncePerRequestFilter {
     @Autowired
     private DetallesDeUsuarioServicio detallesDeUsuarioServicio;
